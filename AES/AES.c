b30df630204480f049125b7d59dc2a820affc4ec
bbelandr <bbelandr@vols.utk.edu>
2024-09-08 17:43:45 -0400

    Working Key Expansion

--- AES/AES.c
+++ AES/AES.c
@@ -14,7 +14,7 @@ uint8_t ffAdd(uint8_t a, uint8_t b) {
 
 // Multiplies a polynomial (represented by 8 bits) by x and modulo's
 // it to make sure that it stays within GF(2^8)
-uint8_t xtime(uint8_t bits) {   
+uint8_t xtime(uint8_t bits) {
     uint16_t temp = bits << 1;
     if (GETBIT(temp, 8)) {
         temp = temp ^ 0x1b;
@@ -60,13 +60,13 @@ uint8_t subByte(uint8_t byte) {
         { 0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf },
         { 0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 }
     };
-    
+
     return SBOX[byte >> 4][byte & 0b1111];
 }
 
 // Takes a pointer to a four bit word and substitues each corresponding byte with S-Box
 void subWord(uint8_t* word) {
-    
+
     for (int i = 0; i < 4; i++) {
         word[i] = subByte(word[i]);
     }
@@ -91,6 +91,7 @@ void subBytes(uint8_t* state) {
     }
 }
 
+// Helper function for shiftRows
 void shiftRow(uint8_t* state, int row) {
 
     uint8_t lastByte = state[row + 3 * 4];
@@ -113,8 +114,102 @@ void shiftRows(uint8_t* state) {    // This is slow but I'm just trying to get s
     shiftRow(state, 3);
 }
 
+void mixColumns(uint8_t* state) {
+    const uint8_t poly[4][4] = {
+        {0x02, 0x03, 0x01, 0x01},
+        {0x01, 0x02, 0x03, 0x01},
+        {0x01, 0x01, 0x02, 0x03},
+        {0x03, 0x01, 0x01, 0x02}
+    };
+
+    uint8_t tmpCol[4];
+    for (int k = 0; k < 4; k++) {
+        for (int i = 0; i < 4; i++) {
+            tmpCol[i] = 0;
+            for (int j = 0; j < 4; j++) {
+                tmpCol[i] = ffAdd(ffMultiply(poly[i][j], state[k * 4 + j]), tmpCol[i]);
+                // state[i * 4 + j] = ffMultiply(0x02, 0xd4);
+
+            }
+        }
+
+        // Copy over tmpCol to state
+        for (int i = 0; i < 4; i++) {
+            state[k * 4 + i] = tmpCol[i];
+        }
+    }
+
+}
+
+void addRoundKey(uint8_t* state, uint8_t* roundKey) {
+    for (int i = 0; i < 16; i++) {
+        state[i] = ffAdd(state[i], roundKey[i]);
+    }
+}
+
+void invSubBytes(uint8_t* state) {
+    const uint8_t INVSBOX[16][16] = {
+        { 0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb } ,
+        { 0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb } ,
+        { 0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e } ,
+        { 0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25 } ,
+        { 0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92 } ,
+        { 0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84 } ,
+        { 0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06 } ,
+        { 0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b } ,
+        { 0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73 } ,
+        { 0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e } ,
+        { 0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b } ,
+        { 0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4 } ,
+        { 0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f } ,
+        { 0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef } ,
+        { 0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61 } ,
+        { 0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d }
+    };
+    for (int i = 0; i < 16; i++) {
+        state[i] = INVSBOX[state[i] >> 4][state[i] & 0b1111];
+    }
+}
+
+void invShiftRows(uint8_t* state) {
+    shiftRow(state, 3);
+    shiftRow(state, 2);
+    shiftRow(state, 2);
+    shiftRow(state, 1);
+    shiftRow(state, 1);
+    shiftRow(state, 1);
+}
+
+void invMixColumns(uint8_t* state) {
+    const uint8_t poly[4][4] = {
+        {0x0e, 0x0b, 0x0d, 0x09},
+        {0x09, 0x0e, 0x0b, 0x0d},
+        {0x0d, 0x09, 0x0e, 0x0b},
+        {0x0b, 0x0d, 0x09, 0x0e}
+    };
+
+    uint8_t tmpCol[4];
+    for (int k = 0; k < 4; k++) {
+        for (int i = 0; i < 4; i++) {
+            tmpCol[i] = 0;
+            for (int j = 0; j < 4; j++) {
+                tmpCol[i] = ffAdd(ffMultiply(poly[i][j], state[k * 4 + j]), tmpCol[i]);
+                // state[i * 4 + j] = ffMultiply(0x02, 0xd4);
+
+            }
+        }
+
+        // Copy over tmpCol to state
+        for (int i = 0; i < 4; i++) {
+            state[k * 4 + i] = tmpCol[i];
+        }
+    }
+
+
+}
+
+// Expands the key to the 10, 12, or 14 round keys (depending on 128, 192, or 256)
 void keyExpansion(const uint8_t* key, uint8_t* w, const int Nk) {
-    
     const uint32_t Rcon[52] = { 0x00000000,
            0x01000000, 0x02000000, 0x04000000, 0x08000000,
            0x10000000, 0x20000000, 0x40000000, 0x80000000,
@@ -145,6 +240,7 @@ void keyExpansion(const uint8_t* key, uint8_t* w, const int Nk) {
             for (int i = 0; i < 4; i++) {
                 copy[i] = w[(col - 1) * 4 + i];
             }
+
             rotWord(copy);
             for (int i = 0; i < 4; i++) {
                 copy[i] = subByte(copy[i]);
@@ -152,7 +248,7 @@ void keyExpansion(const uint8_t* key, uint8_t* w, const int Nk) {
 
             // XOR with Rcon
             // printf("%.2x %.2x\n", copy[0], Rcon[col/4] >> 24);
-            copy[0] = copy[0] ^ (Rcon[col/4] >> 24);  // The - Nk is probably wrong
+            copy[0] = copy[0] ^ (Rcon[col/4] >> 24); 
             for (int i = 0; i < 4; i++) {
                 copy[i] = copy[i] ^ w[(col - 4) * 4 + i];
             }
@@ -161,9 +257,13 @@ void keyExpansion(const uint8_t* key, uint8_t* w, const int Nk) {
             for (int i = 0; i < 4; i++) {
                 w[(col) * 4 + i] = copy[i];
             }
-            // w[col] = copy
-            // printf("%.2d\n", col);
 
+        }
+        else {
+            // XOR by w[col - 1] and w[col - Nk]
+            for (int i = 0; i < 4; i++) {
+                w[col*4 + i] = w[(col - 1)*4 + i] ^ w[(col - Nk)*4 + i];
+            }
         }
     }
 
@@ -173,8 +273,8 @@ void keyExpansion(const uint8_t* key, uint8_t* w, const int Nk) {
 int main() {
     // TEST CASES
     // printf("%x\n", ffMultiply(0x57, 0x13));
-    
-    // uint8_t* column = calloc(4, sizeof(uint8_t));
+
+    // uint8_t* column = (uint8_t*)calloc(4, sizeof(uint8_t));
     // column[0] = 0x19;
     // column[1] = 0x3d;
     // column[2] = 0xe3;
@@ -184,38 +284,67 @@ int main() {
     //     printf("%x\n", column[i]);
     // }
     // free(column);
-
-    const int Nk = 4;
-
-    uint8_t* state = calloc(16, sizeof(uint8_t));
-    uint8_t* w = calloc(4 * (4 * ((Nk + 6) + 1)), sizeof(uint8_t));   // This will just store the bytes, not the columns
-    const uint8_t stateArr[16] = {
-        0x19, 0x3d, 0xe3, 0xbe, 
-        0xa0, 0xf4, 0xe2, 0x2b, 
-        0x9a, 0xc6, 0x8d, 0x2a, 
-        0xe9, 0xf8, 0x48, 0x08
-    };
+    uint8_t* w;
+    int Nk = 4;
     const uint8_t key[16] = {
         0x2b, 0x7e, 0x15, 0x16,
         0x28, 0xae, 0xd2, 0xa6,
         0xab, 0xf7, 0x15, 0x88,
         0x09, 0xcf, 0x4f, 0x3c
     };
+    w = calloc(4 * 4 * (Nk + 6 + 1), sizeof(uint8_t));  // 4*4 blocks * num of Rounds
+    keyExpansion(key, w, Nk);
+
+    // Printing w
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < (4 * ((Nk + 6) + 1)); j++) {
+            printf("%.2x ", w[i + j * 4]);   // i*(4 * (Nk + 6 + 1))
+            if (j % 4 == 3) {
+                printf("    ");
+            }
+        }
+        printf("\n");
+    }
+
+
+    uint8_t* state = (uint8_t*)calloc(16, sizeof(uint8_t));
+    uint8_t* roundKey = (uint8_t*)calloc(16, sizeof(uint8_t));
+    const uint8_t stateArr[16] = {
+        0x19, 0x3d, 0xe3, 0xbe,
+        0xa0, 0xf4, 0xe2, 0x2b,
+        0x9a, 0xc6, 0x8d, 0x2a,
+        0xe9, 0xf8, 0x48, 0x08
+    };
+    const uint8_t keyArr[16] = {
+        0xa0, 0xfa, 0xfe, 0x17,
+        0x88, 0x54, 0x2c, 0xb1,
+        0x23, 0xa3, 0x39, 0x39,
+        0x2a, 0x6c, 0x76, 0x05
+    };
 
     for (int i = 0; i < 16; i++) {
         state[i] = stateArr[i];
+        roundKey[i] = keyArr[i];
     }
+
 
     // subBytes(state);
     // shiftRows(state);
-    keyExpansion(key, w, Nk);
-    
+    // mixColumns(state);
+    // addRoundKey(state, roundKey);
+    // addRoundKey(state, roundKey);
+    // invMixColumns(state);
+    // invShiftRows(state);
+    // invSubBytes(state);
+
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             printf("%.2x ", state[j * 4 + i]);
         }
         printf("\n");
     }
-    free(state);
+
     free(w);
+    free(state);
+    free(roundKey);
 }
