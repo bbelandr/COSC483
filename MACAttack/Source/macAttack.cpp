6d0d2a152958b1cd3aea73db047ebd2a5e9a7731
bbelandr <bbelandr@vols.utk.edu>
2024-09-28 02:19:24 -0400

    working CMake compilation with Boost Library

--- MACAttack/Source/macAttack.cpp
+++ MACAttack/Source/macAttack.cpp
@@ -1,12 +1,21 @@
 // MAC ATTACK, Benjamin Belandres
-// #include <boost/dynamic_bitset.hpp>
 #include <iostream>
-
-// HELPER FUNCTIONS
+#include <boost/dynamic_bitset/dynamic_bitset.hpp>
 
 using namespace std;
 
+// HELPER FUNCTIONS
+
+
 int main() {
-    cout << "Hello World" << endl;
+    cout << "Hello asdkfj" << endl;
+    boost::dynamic_bitset<> B1;
+
+    B1.push_back(1);
+    B1.push_back(0);
+    B1.push_back(1);
+
+    cout << B1 << endl;
+    
     return 0;
 }
